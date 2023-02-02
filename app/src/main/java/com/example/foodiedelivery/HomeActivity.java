a85829550295e8aa162c877cc293f7debca88458
Macy Fong <macyfong525@gmail.com>
2023-02-01 21:59:02 -0800

    add google sign in

---
+++ app/src/main/java/com/example/foodiedelivery/HomeActivity.java
@@ -0,0 +1,56 @@
+package com.example.foodiedelivery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.Task;
+
+public class HomeActivity extends AppCompatActivity {
+
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
+    TextView name, email;
+    Button signOutBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        name = findViewById(R.id.textViewName);
+        email = findViewById(R.id.textViewEmail);
+        signOutBtn = findViewById(R.id.signOutBtn);
+
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        gsc = GoogleSignIn.getClient(this, gso);
+
+        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
+        if(acct != null){
+            String userName = acct.getDisplayName();
+            String userEmail = acct.getEmail();
+            name.setText(userName);
+            email.setText(userEmail);
+        }
+
+        signOutBtn.setOnClickListener((View view)->{
+            signOut();
+        });
+
+    }
+
+    void signOut(){
+        gsc.signOut().addOnCompleteListener((Task<Void> task)->{
+            finish();
+            startActivity(new Intent(HomeActivity.this, LoginActivity.class));
+        });
+    }
+}
---
+++ app/src/main/java/com/example/foodiedelivery/LoginActivity.java
@@ -0,0 +1,87 @@
+package com.example.foodiedelivery;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
+public class LoginActivity extends AppCompatActivity {
+
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
+    ImageView googleBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        googleBtn = findViewById(R.id.googleBtn);
+
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        gsc = GoogleSignIn.getClient(this, gso);
+
+        googleBtn.setOnClickListener((View view)->{
+            signIn();
+        });
+    }
+
+    void signIn(){
+        Intent signInIntent = gsc.getSignInIntent();
+        googleLauncher.launch(signInIntent);
+    }
+
+    ActivityResultLauncher<Intent> googleLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                        Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+
+                        try {
+                            task.getResult(ApiException.class);
+                            // navigate to home
+                            Intent intent  = new Intent(LoginActivity.this, HomeActivity.class);
+                            startActivity(intent);
+                        } catch (ApiException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            });
+
+
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        if(resultCode == 200){
+//            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+//
+//            try {
+//                task.getResult(ApiException.class);
+//                // navigate to home
+//                Intent intent  = new Intent(LoginActivity.this, HomeActivity.class);
+//                startActivity(intent);
+//            } catch (ApiException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
+}
