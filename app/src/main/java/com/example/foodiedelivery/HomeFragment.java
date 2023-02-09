8d3d4fae31f11c1e23f85dc853a3c2a44a4b0f31
hon <hon.tnguyen294@gmail.com>
2023-02-09 14:31:09 -0800

    Messaged by Hon

--- app/src/main/java/com/example/foodiedelivery/HomeFragment.java
+++ app/src/main/java/com/example/foodiedelivery/HomeFragment.java
@@ -9,7 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class HomeFragment extends Fragment {
-
+   // test comment
    @Override
    public View onCreateView(LayoutInflater inflater, ViewGroup container,
                             Bundle savedInstanceState) {
