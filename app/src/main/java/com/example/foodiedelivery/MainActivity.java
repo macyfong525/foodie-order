6917d937cf7b65f3e674d5320819a19978b17847
hon <hon.tnguyen294@gmail.com>
2023-02-11 00:24:37 -0800

    updated new top fragments

--- app/src/main/java/com/example/foodiedelivery/MainActivity.java
+++ app/src/main/java/com/example/foodiedelivery/MainActivity.java
@@ -26,17 +26,45 @@ public class MainActivity extends AppCompatActivity {
    NotificationFragment notificationFragment = new NotificationFragment();
    SettingsFragment settingsFragment = new SettingsFragment();
 
+   RedFragment redFragment = new RedFragment();
+   GreenFragment greenFragment = new GreenFragment();
+   LogoutFragment logoutFragment =new LogoutFragment();
+
    RelativeLayout relativeLayout;
+
+
+   @Override
+   public boolean onCreateOptionsMenu(Menu menu) {
+      MenuInflater inflater = getMenuInflater();
+      inflater.inflate(R.menu.app_menu,menu);
+      return true;
+   }
+
+   @Override
+   public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+      switch (item.getItemId()){
+         case R.id.red:
+            relativeLayout.setBackgroundColor(Color.RED);break;
+         case R.id.green:
+            relativeLayout.setBackgroundColor(Color.GREEN);break;
+         case R.id.logout:
+            Toast.makeText(this,"YOU'RE SUCCESSFULLY LOGGED OUT",Toast.LENGTH_SHORT).show();
+            break;
+      }
+      return true;
+   }
 
    @Override
    protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_main);
 
+      relativeLayout =findViewById(R.id.rel_layout);
+
 
       bottomNavigationView = findViewById(R.id.bottom_navigation);
       getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment).commit();
-      relativeLayout =findViewById(R.id.rel_layout);
+
       BadgeDrawable badgeDrawable = bottomNavigationView.getOrCreateBadge(R.id.notification);
       badgeDrawable.setVisible(true);
       badgeDrawable.setNumber(8);
